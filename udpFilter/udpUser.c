b279304248dcefaf1e0545d612eb5cd1a637f252
Joao Trindade <trindade.joao@gmail.com>
2010-04-19 17:02:34 +0100

    The udp client now is commented

--- udpFilter/udpUser.c
+++ udpFilter/udpUser.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in receivesocket;
 	struct sockaddr_in sendsocket;
 	int sock;
-	
+
 	int ret = 0;
 
 	/* Create the UDP socket */
@@ -19,20 +19,21 @@ int main(int argc, char *argv[]) {
 		perror("socket");
 		return -1;
 	}
+	/* Now lets prepate two sockets. One to receive and one to send */
 
-	/* my address */
-	memset(&receivesocket, 0, sizeof(receivesocket));  
-	receivesocket.sin_family = AF_INET; 
+        /* To receive -> My address */
+	memset(&receivesocket, 0, sizeof(receivesocket));
+	receivesocket.sin_family = AF_INET;
 	receivesocket.sin_addr.s_addr = htonl(INADDR_ANY);
 	receivesocket.sin_port = htons(9999);
 
 	receivelen = sizeof(receivesocket);
 	if (bind(sock, (struct sockaddr *) &receivesocket, receivelen) < 0) {
-		perror("bind");
-		return -1;		
+		perror("Failed to bind to local address");
+		return -1;
 	}
 
-	/* kernel address */
+	/* To send -> kernel address */
 	memset(&sendsocket, 0, sizeof(sendsocket));
 	sendsocket.sin_family = AF_INET;
 	sendsocket.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -41,19 +42,20 @@ int main(int argc, char *argv[]) {
 	/* Send message to the server */
 	memcpy(buffer, "hello world", strlen("hello world") + 1);
 	sendlen = strlen(buffer) + 1;
-	
+
 	if (sendto(sock, buffer, sendlen, 0, (struct sockaddr *) &sendsocket, sizeof(sendsocket)) != sendlen) {
-		perror("sendto");
+		perror("Sending the mesage failed");
 		return -1;
 	}
 
-	memset(buffer, 0, BUFFSIZE);
+	memset(buffer, 0, BUFFSIZE); // Prepare the buffer to receive the response
+        /* Receive the response */
 	if ((received = recvfrom(sock, buffer, BUFFSIZE, 0, NULL, NULL)) < 0){
 		perror("recvfrom");
 		return -1;
 	}
-	
-	printf("message received: %s\n", buffer);
+
+	printf("message received from the server: %s\n", buffer);
 
 	return 0;
 }
